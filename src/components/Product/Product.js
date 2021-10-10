a09ef0691b64b545ef8acc8e90b10a712d57bafa
Jahid Hasan <jahidhasan2856@gmail.com>
2021-10-10 08:56:43 +0600

    dynamic variation search product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,31 @@
-import { faMinus, faPlus, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import React, { useState } from 'react';
 import './Product.css'
-const Product = ({ product, combination, handleQuantity, quanity}) => {
+const Product = ({ product, combination, handleQuantity, quanity, selectedVariation }) => {
 
-    const [storageSelect, setStorageSelect] = useState(null)
-  const handleStorage = (e) => {
-     const value = e.target.value;
-  
-  }
+    const [allVariation, setAllVariation] = useState({ storage: 3480, color: 3483, sim: 3487, region: 3489 })
+
+    
+    selectedVariation(allVariation);
+    
+    const selectVariation = (id, combName) => {
+        if (combName === 'storage') {
+            
+            setAllVariation({...allVariation, storage: id})
+
+        } else if (combName === 'color') {
+           
+            setAllVariation({...allVariation, color: id})
+        } else if (combName === 'sim') {
+           
+            setAllVariation({...allVariation, sim: id})
+        } else if (combName === 'region') {
+           
+            setAllVariation({...allVariation, region: id})
+        }
+
+    }
+    // selectedVariation(storageSelect.values[0], selectColor.values[0], selectSim.values[0], selectRegion.values[0])
     return (
         <div className="product-area d-flex">
 
@@ -23,47 +40,57 @@ const Product = ({ product, combination, handleQuantity, quanity}) => {
                         <li><span>Storage:</span>
                             <div className="variation-1">
                                 {
-                                combination.length && combination[0].Storage.map(storage => 
-                                <button onClick={(e) => handleStorage(e)} value={storage.id} key={storage.id} className ="select-btn">{storage.value}</button>
-                                )
+                                    combination.length && combination[0].Storage.map(storage =>
+                                        <button onClick={() => selectVariation(storage.id, 'storage')}
+
+                                            key={storage.id} className={allVariation.storage === storage.id ? "selected select-btn" : "select-btn"}>
+                                            {storage.value}</button>
+                                    )
                                 }
                             </div>
                         </li>
                         <li><span>Color:</span>
                             <div className="variation-2 d-flex">
-                                <div className="border rounded-circle me-2">
-                                    <div className="color-box m-1 d-flex justify-content-center align-items-center"><span>1</span></div>
-                                </div>
-                                <div className="border rounded-circle me-2">
-                                    <div className="color-box m-1 d-flex justify-content-center align-items-center"><span>1</span></div>
-                                </div>
-                                <div className="border rounded-circle me-2">
-                                    <div className="color-box m-1 d-flex justify-content-center align-items-center"><span>1</span></div>
-                                </div>
-                                <div className="border rounded-circle me-2">
-                                    <div className="color-box m-1 d-flex justify-content-center align-items-center"><span>1</span></div>
-                                </div>
+                                {combination.length && combination[1].Color.map(color =>
+                                    <div onClick={() => selectVariation(color.id, 'color')} key={color.id} className={allVariation.color === color.id ? "selected border rounded-circle me-2" : "border rounded-circle me-2"} >
+                                        <div title={color.value.substring(0, color.value.indexOf('_'))} className={`${color.value} color-box m-1 d-flex justify-content-center align-items-center`}></div>
+                                    </div>
+                                )}
 
                             </div>
                         </li>
                         <li><span>Sim:</span>
                             <div className="variation-3">
-                                <button className="select-btn">e-Sim</button>
-                                <button className="select-btn">Dual</button>
+                                {
+                                    combination.length && combination[2].Sim.map(sim =>
+                                        <button onClick={() => selectVariation(sim.id, 'sim')}
+
+                                            key={sim.id} className={allVariation.sim === sim.id ? "selected select-btn" : "select-btn"}>
+                                            {sim.value}</button>
+                                    )
+                                }
+
 
                             </div>
                         </li>
                         <li><span>Region:</span>
                             <div className="variation-4">
-                                <button className="select-btn">Regular</button>
-                                <button className="select-btn">USA</button>
+                                {
+                                    combination.length && combination[3].Region.map(region =>
+                                        <button onClick={() => selectVariation(region.id, 'region')}
+
+                                            key={region.id} className={allVariation.region === region.id ? "selected select-btn" : "select-btn"}>
+                                            {region.value}</button>
+                                    )
+                                }
+
 
                             </div>
                         </li>
-                        <li>Quatity: 
+                        <li>Quatity:
                             <div className="quantity">
-                                <button onClick={ () => handleQuantity(true)} className="m-1">+</button>
-                                <input className="m-1 text-center" type="text" value={quanity} onScroll="none"/>
+                                <button onClick={() => handleQuantity(true)} className="m-1">+</button>
+                                <input className="m-1 text-center" type="text" value={quanity} onScroll="none" />
                                 <button onClick={() => handleQuantity(false)} className="m-1">-</button>
                             </div>
                         </li>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,33 +2,60 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 const Products = () => {
     const [products, setProducts] = useState([]);
-    const [combination, setCombination] = useState([])
-    const [quanity, setQuantity] = useState(1)
+    const [combination, setCombination] = useState([]);
+    const [quanity, setQuantity] = useState(1);
+
+
     useEffect(() => {
         const variation = 'https://raw.githubusercontent.com/qtecsolution/Sample-JSON/main/variation.json'
         fetch(variation)
-        .then(res => res.json())
-        .then(data => setProducts(data));
+            .then(res => res.json())
+            .then(data => setProducts(data));
 
         const combination = 'https://raw.githubusercontent.com/qtecsolution/Sample-JSON/main/combination.json'
         fetch(combination)
-        .then(res => res.json())
-        .then(data => setCombination(data))
+            .then(res => res.json())
+            .then(data => setCombination(data))
     }, [])
 
     const handleQuantity = (isTrue) => {
-       if(isTrue){
-           setQuantity(quanity + 1)
-       }
-       else if(quanity > 1){
-           setQuantity(quanity - 1)
-       }
+        if (isTrue) {
+            setQuantity(quanity + 1)
+        }
+        else if (quanity > 1) {
+            setQuantity(quanity - 1)
+        }
     }
+
+    const [newProduct, setNewProduct] = useState([])
+    const selectVariation = (vari) => {
+        const comb = [vari.storage, vari.color, vari.sim, vari.region]
+
+        products.some((pd, index) => {
+            return pd.attribute_combination.every((prop, i) => {
+            if (comb[i] === prop) {
+               return  setNewProduct(products[index])
+                };
+               return false;
+            })
+            
+        });
+    }
+    
+    console.log(newProduct)
+  
     return (
         <section>
             <div className="container">
                 <div className="row">
-                    {products.map(pd => <Product handleQuantity={handleQuantity} quanity={quanity} product={pd} key={pd.id} combination={combination}></Product>)}
+                    {<Product
+                        selectedVariation={selectVariation}
+                        handleQuantity={handleQuantity}
+                        quanity={quanity}
+                        product={newProduct}
+                        
+                        combination={combination}>
+                    </Product>}
                 </div>
             </div>
         </section>
